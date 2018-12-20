8261a018a2714a80e65ee95feab4f55e5dc4b031
SUSTC-ChuangYANG <1360136415@qq.com>
2018-12-21 01:00:56 +0800

    beat 20.76% of Python3. Ranking: Hard

--- slidingPuzzle.py
+++ slidingPuzzle.py
@@ -1,104 +1,77 @@
 class Solution:
-    met = {}
     def slidingPuzzle(self, board):
         """
         :type board: List[List[int]]
         :rtype: int
         """
+        import copy
         initial = tuple([item for row in board for item in row])
-        for i,row in enumerate(board):
-            for j,item in enumerate(row):
-                if item == 0:
-                    start_position = (i,j)
-        initial_status = {'status':initial,'pos':start_position}
-        depth = 0
-        candidate = [initial_status]
-        met = {initial}
-        while candidate:
-            x = candidate.pop()
-            cur_pos = x[pos]
-            if self.is_valid_position((cur_pos[0]-1,cur_pos[1]-1)): #  左上
-
-
-        if is_valid(initial):
-            print("他妈的都不用找，找个锤子")
+        if initial == (1,2,3,4,5,0):
             return 0
-
+        start_position = None
         for i,row in enumerate(board):
             for j,item in enumerate(row):
                 if item == 0:
                     start_position = (i,j)
 
+        initial_status = {'status': initial,'pos': start_position,'board': copy.deepcopy(board),'depth':0}
+        candidates = [initial_status]
+        met = {initial}
 
-        # 这才是正常道路
-        met.add(initial)
-        return self.search(board, met, depth=0, start_position=start_position)
+        while candidates:
+            x = candidates.pop(0)
+            pos, board, depth = x['pos'], x['board'], x['depth']
+            if self.is_valid_position((pos[0], pos[1]-1)): #  左上
+                new_status, new_board, new_pos = \
+                    self.swap(copy.deepcopy(board),(pos[0],pos[1]-1),pos)
+                if new_status == (1,2,3,4,5,0):
+                    return depth+1
+                if new_status not in met:
+                    met.add(new_status)
+                    candidates.append({'status':new_status,'pos':new_pos,'board':new_board,'depth':depth+1})
 
-    def search(self, board, met, depth, start_position):
-        # 左上角
-        left_up = (start_position[0]-1, start_position[1]-1)
-        # 该位置有效
-        if self.is_valid_position(left_up):
-            # 交换
-            self.swap(board=board, start_position = start_position,des_position=left_up)
-            #
-            current = self.is_met(board=board, met=met)
-            # 如果当前的局势已经出现过，
-                    # 这是一个可达路径， 直接进行深度拼接
-                    # 这是一个环，剪掉
+            if self.is_valid_position((pos[0], pos[1]+1)): #  左上
+                new_status, new_board, new_pos = \
+                    self.swap(copy.deepcopy(board), (pos[0],pos[1]+1),pos)
+                if new_status == (1,2,3,4,5,0):
+                    return depth+1
+                if new_status not in met:
+                    met.add(new_status)
+                    candidates.append({'status':new_status,'pos':new_pos,'board':new_board,'depth':depth+1})
 
-            # 如果当前的局势从未见过
-            if current:
-                met.add(current)
-                return self.search(board, met, depth=depth+1, start_position=left_up)
-            else:
-                self.swap(board=board, start_position=left_up, des_position=start_position)
-
-        if self.is_valid_position(start_position):
-
-            self.search()
-        else:
-            return
-        empty_position = None
-        for i,row in enumerate(board):
-            for j,item in enumerate(row):
-                if item == 0:
-                    empty_position = (i,j)
-                    break
-        if empty_position[0]-1 > 0 and empty_position[1]-1 > 0:  # 左上角
-
-            x = board[empty_position[0]-1][empty_position[1]-1]
-            board[empty_position[0] - 1][empty_position[1] - 1] = 0
-            board[empty_position[0]][empty_position[1]] = x
-            current = tuple([item for row in board for item in row])
-            if current not in met:
-                met.add(current)
-                if is_vaild(current):
-                    return step
-        # if empty_position[0]-1>=0 and empty_position[1]-1>=0
+            if self.is_valid_position((pos[0]-1, pos[1])): #  左上
+                new_status, new_board, new_pos = \
+                    self.swap(copy.deepcopy(board),(pos[0]-1,pos[1]),pos)
+                if new_status == (1,2,3,4,5,0):
+                    return depth+1
+                if new_status not in met:
+                    met.add(new_status)
+                    candidates.append({'status':new_status,'pos':new_pos,'board':new_board,'depth':depth+1})
+            if self.is_valid_position((pos[0]+1, pos[1])): #  左上
+                new_status, new_board, new_pos = \
+                    self.swap(copy.deepcopy(board),(pos[0]+1,pos[1]),pos)
+                if new_status == (1,2,3,4,5,0):
+                    return depth+1
+                if new_status not in met:
+                    met.add(new_status)
+                    candidates.append({'status':new_status,'pos':new_pos,'board':new_board,'depth':depth+1})
+        return -1
 
     def is_valid_position(self,position):
-
-        if 0 <= position[0] <= 1 and 0 <= position[1] <=2:
+        if 0 <= position[0] <= 1 and 0 <= position[1] <= 2:
             return True
         else:
             return False
-    def swap(self,board, source_position, des_position):
+    def swap(self,board, source_position,des_position): # 把源位置挪到新的空位上去
         x = board[source_position[0]][source_position[1]]
         board[source_position[0]][source_position[1]] = 0
         board[des_position[0]][des_position[1]] = x
-
-    def is_met(self,board, met):
-        current = tuple([item for row in board for item in row])
-        if current in met:
-            return None
-        else:
-            return current
+        current = tuple(board[0]) + tuple(board[1])
+        return current, board, source_position
 
 
 
 
 
 s = Solution()
-# s.slidingPuzzle([[1,3,3],[4,5,0]])
-print(s.valid_position((0,-1)))
+print(s.slidingPuzzle([[3,2,4],[1,5,0]]))
