57a6d9264c809aaffccd463b2716faaeaa638487
SUSTC-ChuangYANG <1360136415@qq.com>
2018-12-23 18:37:58 +0800

    TODO Understand the relationship between stack and recursion. To make myself easily replace a recursion procedure with stack.

---
+++ TraversalTree/postorderTraversal.py
@@ -0,0 +1,40 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def postorderTraversal(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[int]
+        """
+		res = []
+		self.recursion_version(root,res)
+		return res
+
+	def recursion_version(self, root, res):
+		if root:    		
+		self.recursion_version(root.left, res)
+		self.recursion_version(root.right,res)
+		res.append(root.val)
+
+	def stack_version(self, root, res):
+        if not root: return
+        stack = [root]
+        while stack:
+            while root:
+                if root.left: stack.append(root.left)
+                root = root.left
+            node = stack.pop()		
+            if not node.right:
+                res.append(node.val)
+                root = None
+            else:
+                stack.append(node)
+                root = node.right
+                node.right = None
+                stack.append(root)
+			
