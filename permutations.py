e6943be83b5e9c7aeb3aefe9a862c71e42d0403c
SUSTC-ChuangYANG <1360136415@qq.com>
2019-01-26 17:49:33 +0800

    48 ms, faster than 80.57% of Python3 online submissions for Permutations.

---
+++ permutations.py
@@ -0,0 +1,28 @@
+class Solution:
+    def permute(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        res_l = 1
+        for i in range(1,len(nums)+1):
+            res_l *= i
+        res = [[] for i in range(res_l)]
+        self.back_tracking(res, nums, 0, res_l)
+        print(res)
+        return res
+
+    def back_tracking(self, res, nums, start, batch_size):
+        length = len(nums)
+        if length == 1:
+            res[start].append(nums[0])
+            return
+        batch_size = int(batch_size / length)
+        for i, num in enumerate(length):
+            new_start = start+i*batch_size
+            self.back_tracking(res, nums[0:i] + nums[i + 1:], start=new_start, batch_size=batch_size)
+            for idx in range(new_start,new_start+batch_size):
+                res[idx].append(num)
+
+s = Solution()
+s.permute([1,2,3,4])
